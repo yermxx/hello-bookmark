da3ccc31c8048e298fa2cea21d903bda79c9c054
yermxx <66yrlee99@gmail.com>
2024-12-18 01:40:09 +0900

    feat: add UX improvements to ItemEditor component

--- src/components/common/molecules/ItemEditor.tsx
+++ src/components/common/molecules/ItemEditor.tsx
@@ -4,20 +4,50 @@ import {
   HiArrowUturnRight,
   HiMiniArchiveBoxXMark,
 } from 'react-icons/hi2';
-import { FormEvent } from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
 
 export default function ItemEditor({ onClose }: { onClose: () => void }) {
-  const handlerSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const itemRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    containerRef.current?.scrollIntoView({
+      behavior: 'smooth',
+    });
+    inputRef.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        itemRef.current &&
+        e.target instanceof Node &&
+        !itemRef.current.contains(e.target)
+      ) {
+        onClose();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
 
   return (
-    <div className='border border-black rounded-lg'>
-      <form onSubmit={handlerSubmit} className='p-4'>
-        <div className='flex flex-col mb-4'>
+    <div ref={itemRef} className='border border-black rounded-lg'>
+      <form onSubmit={handleSubmit} className='p-4'>
+        <div ref={containerRef} className='flex flex-col mb-4'>
           <label>URL</label>
           <div className='grid grid-cols-4 gap-3'>
-            <input className='border border-gray-400 rounded-md px-2 col-span-3' />
+            <input
+              ref={inputRef}
+              className='border border-gray-400 rounded-md px-2 col-span-3'
+            />
             <button className='col-span-1 px-4 rounded-lg border border-black mx-2'>
               <HiArrowUturnRight />
             </button>

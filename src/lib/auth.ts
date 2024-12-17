e395ee3e21feabb78ffd45287caa5c88ccb2ba52
yermxx <66yrlee99@gmail.com>
2024-12-18 00:04:02 +0900

    feat: Add Next-auth base configuration

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [Google, Github],
+});

a3d5b528dc5c7d5a370f36e6019c837dc9a16fca
yermxx <66yrlee99@gmail.com>
2024-12-20 01:26:53 +0900

    feat: Update Nav component for Next-auth

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,52 @@
-import Image from 'next/image';
-import Tokki from '../public/tokki.jpeg';
+import { BsBookmarkHeart } from 'react-icons/bs';
+import Link from 'next/link';
+import { auth } from '@/lib/auth';
 
-export default function Nav() {
+// import Image from 'next/image';
+// import Tokki from '../public/tokki.jpeg';
+
+export default async function Nav() {
+  const session = await auth();
+  const didLogin = !!session?.user;
+  console.log(session);
+
   return (
     <div className='flex items-center justify-between'>
-      <div>
-        <p className='text-xl px-6'>Rimi&apos;s Bookmark</p>
-      </div>
-      <div className='flex'>
-        <button className='hover:text-gray-500'>Github Repo.</button>
-        <Image
+      <Link href='/'>
+        <div className='flex items-center justify-between'>
+          <p className='text-xl pl-6 pr-1'>Rimi&apos;s Bookmark</p>
+          <BsBookmarkHeart />
+        </div>
+      </Link>
+
+      <div className='flex gap-3 m-3'>
+        <Link href='https://github.com/yermxx/bookmark'>
+          <button className='px-2.5 py-1 border rounded-full border-black hover:border-gray-500 hover:text-gray-500'>
+            Github Repo.
+          </button>
+        </Link>
+        {didLogin ? (
+          <Link
+            href='/api/auth/signout'
+            className='bg-black text-white px-2.5 py-1 rounded-full hover:bg-gray-800'
+          >
+            Hello! {session.user?.name}
+          </Link>
+        ) : (
+          <Link
+            href='/api/auth/signin'
+            className='px-2.5 py-1 border rounded-full border-black hover:border-gray-400 hover:text-gray-400'
+          >
+            Sign In
+          </Link>
+        )}
+        {/* <Image
           src={Tokki}
           alt='토끼'
           width='30'
           height='30'
           className='m-3 border border-gray-400 rounded-3xl h-auto'
-        />
+        /> */}
       </div>
     </div>
   );
